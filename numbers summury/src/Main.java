dd521a5f1345861dfc4530d8304871871fb80bf3
ann <40922431+apanovich1@users.noreply.github.com>
2023-09-04 21:03:55 +0200

    exercise loops -4

--- numbers summury/src/Main.java
+++ numbers summury/src/Main.java
@@ -1,31 +1,23 @@
+/*Напишите программу, где пользователь вводит любое целое
+положительное число. А программа суммирует все числа от 1 до
+введенного  пользователем числа. Для ввода числа воспользуйтесь
+классом Scanner.
+*/
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int userInput = scanner.nextInt();
-
         int sum = 0;
-
-        while (true) {
-            System.out.print("Enter another number (or the same number to stop): ");
-            int number = scanner.nextInt();
-
-            if (number == userInput) {
-                break; // Exit the loop if the user enters the same number
-            }
-
-            sum += number; // Add the entered number to the sum
+        for(int i = 0;i<userInput;i++){
+            sum += i;
         }
-
-        scanner.close();
-
-        System.out.println("Sum of numbers located before " + userInput + " is: " + sum);
+        System.out.println(sum);
     }
-
 }
+
 
 
 
