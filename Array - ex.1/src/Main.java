3e818595ce819fe710514c7ed2909136ff863967
Hanna Apanovich <anna.apanovich@glassdoor.com>
2023-09-07 11:12:15 +0200

    final variant

--- Array - ex.1/src/Main.java
+++ Array - ex.1/src/Main.java
@@ -1,20 +1,26 @@
+/**Создайте массив целых чисел. Напишете программу, которая
+ выводит сообщение о том, входит ли заданное число в массив или нет.
+ Пусть число для поиска задается с консоли (класс Scanner).
+ */
 import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Integer array[] = { 0,1,2,3,4,5,6,7,8,9,10 };
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int userInput = scanner.nextInt();
 
+        VerifyNumber(array, userInput);
     }
-    public static void VerifyNumber(int userInput){
-        int[] numberArray = new int[10];
-        for(int i=numberArray[0];i<numberArray.length;i++){
-            if(i = userInput){
-                System.out.println("number is in array");
-            }
-            else{
-                System.out.println("number is not in array");
-            }
-        }
+    private static void VerifyNumber(Integer[] array, int userInput)
+    {
+        boolean test
+                = Arrays.asList(array)
+                .contains(userInput);
+
+        System.out.println(test);
     }
 }
