83e9166adc44c1305af61bb0f997e835329e0db2
ann <40922431+apanovich1@users.noreply.github.com>
2023-10-29 18:27:53 +0100

    initial commit

---
+++ src/test/java/ShoppingCartTest.java
@@ -0,0 +1,52 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ShoppingCartTest {
+
+    @Test
+    public void discount0() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py?page=2&zip_code=11111&" +
+                "first_name=test&last_name=testl&email=test%40test.com&password1=test&password2=test");
+        String email = browser.findElement(
+                By.xpath("//table/tbody/tr[6]/td/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/b")).getText();
+        browser.get("https://www.sharelane.com/cgi-bin/main.py");
+        browser.findElement(By.name("email")).sendKeys(email);
+        browser.findElement(By.name("password")).sendKeys("1111");
+        browser.findElement(By.cssSelector("[value=Login]")).click();
+
+        browser.get("https://www.sharelane.com/cgi-bin/add_to_cart.py?book_id=3");
+        browser.get("https://www.sharelane.com/cgi-bin/shopping_cart.py");
+        browser.findElement(By.name("q")).clear();
+        browser.findElement(By.name("q")).sendKeys("1");
+        browser.findElement(By.cssSelector("[value=Update]")).click();
+        String discountPercent = browser.findElement(
+                By.xpath("//table/tbody/tr[6]/td/table/tbody/tr[2]/td[5]/p/b")).getText();
+        String discount$ = browser.findElement(
+                By.xpath("//table/tbody/tr[6]/td/table/tbody/tr[2]/td[6]")).getText();
+        String total$ = browser.findElement(
+                By.xpath("//table/tbody/tr[6]/td/table/tbody/tr[2]/td[7]")).getText();
+        Assert.assertEquals(discountPercent, "0", "discount is incorrect");
+        Assert.assertEquals(discount$, "0.0");
+        Assert.assertEquals(total$, "10.00");
+
+        browser.findElement(By.name("q")).clear();
+        browser.findElement(By.name("q")).sendKeys("19");
+        browser.findElement(By.cssSelector("[value=Update]")).click();
+        discountPercent = browser.findElement(
+                By.xpath("//table/tbody/tr[6]/td/table/tbody/tr[2]/td[5]/p/b")).getText();
+        discount$ = browser.findElement(
+                By.xpath("//table/tbody/tr[6]/td/table/tbody/tr[2]/td[6]")).getText();
+        total$ = browser.findElement(
+                By.xpath("//table/tbody/tr[6]/td/table/tbody/tr[2]/td[7]")).getText();
+        Assert.assertEquals(discountPercent, "0","discount is incorrect");
+        Assert.assertEquals(discount$, "0.0");
+        Assert.assertEquals(total$, "190.00");
+        browser.quit();
+    }
+
+}
---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,91 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SignUpTest {
+
+    @Test
+    public void zipCode4Digits () {
+        //open browser
+        //open page
+        //enter 1234 digits
+        //continue button
+        //error shown
+        //close browser
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("1234");
+        //<input type="text" name="zip_code" value="">
+        //<input type="submit" value="Continue">
+        //<span class="error_message">Oops, error on page. ZIP code should have 5 digits</span>
+        browser.findElement(By.cssSelector("[value=Continue]")).click();
+        String error = browser.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(error,"Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+    @Test
+    public void zipCode6Digits () {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("123456");
+        browser.findElement(By.cssSelector("[value=Continue]")).click();
+        String error = browser.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(error,"Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+    @Test
+    public void zipCodeEmptySubmit () {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.cssSelector("[value=Continue]")).click();
+        String error = browser.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(error,"Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+    @Test
+    public void zipCode5Digits () {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("12345");
+        browser.findElement(By.cssSelector("[value=Continue]")).click();
+        String currentURL = browser.getCurrentUrl();
+        Assert.assertTrue(currentURL.contains("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345")); // Replace with the expected URL
+        browser.quit();
+    }
+    @Test
+    public void positiveSignUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys(new CharSequence[]{"11111"});
+        browser.findElement(By.cssSelector("[value=Continue]")).click();
+        browser.findElement(By.name("first_name")).sendKeys(new CharSequence[]{"Dmitry"});
+        browser.findElement(By.name("last_name")).sendKeys(new CharSequence[]{"Rak"});
+        browser.findElement(By.name("email")).sendKeys(new CharSequence[]{"blabla@gmail.com"});
+        browser.findElement(By.name("password1")).sendKeys(new CharSequence[]{"12345678"});
+        browser.findElement(By.name("password2")).sendKeys(new CharSequence[]{"12345678"});
+        browser.findElement(By.cssSelector("[value=Register]")).click();
+        String signUpMessage = browser.findElement(By.cssSelector("[class=confirmation_message]")).getText();
+        Assert.assertEquals(signUpMessage, "Account is created!");
+        browser.quit();
+    }
+
+    @Test
+    public void negativeSignUpEmptyInput() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys(new CharSequence[]{"11111"});
+        driver.findElement(By.cssSelector("[value = Continue]")).click();
+        driver.findElement(By.cssSelector("[value = Register]")).click();
+        String signUpMessage = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(signUpMessage, "Oops, error on page. Some of your fields have invalid data or email was previously used");
+        driver.quit();
+    }
+}
